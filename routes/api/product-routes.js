027c6768efbea91ca034bc83457b5cd3343f0bc9
Camya <camyacodes@gmail.com>
2022-04-06 11:02:06 -0400

    added product tag model and associations

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -28,6 +28,24 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    // Query configuration
+    where: {
+      id: req.params.id
+    },
+    attributes: ['id', 'product_name', 'price', 'stock'],
+    include: [
+      {
+        model: Category,
+        attributes: ['category_name']
+      }
+    ]
+  })
+  .then(dbProductData => res.json(dbProductData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // create new product
